c8b9984472f536d3660f98105c26afae29d42c72
Alessio Argentati <2006alessio@gmail.com>
2024-01-13 15:08:17 +0100

    First Version

---
+++ CinebookAppAA/CartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CinebookAppAA;
+
+public partial class CartPage : ContentPage
+{
+    public CartPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CinebookAppAA/SearchPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinebookAppAA;
+
+public partial class SearchPage : ContentPage
+{
+    public SearchPage()
+    {
+        InitializeComponent();
+    }
+}
