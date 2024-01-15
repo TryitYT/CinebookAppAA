6cdc729e6831053bc0f1cbc2bad6747a9ed67185
Alessio Argentati <2006alessio@gmail.com>
2024-01-15 06:55:53 +0100

    QA and Search

---
+++ CinebookAppAA/QAPage.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinebookAppAA;
+
+public partial class QAPage : ContentPage
+{
+    public QAPage()
+    {
+        InitializeComponent();
+    }
+}
